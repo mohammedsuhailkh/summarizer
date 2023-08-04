d9c4a552df76f45991fdac8be45f77b55b504037
mohammedsuhailkh <khmohammadsuhail@gmail.com>
2023-08-04 18:05:51 +0530

    initial

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -6,8 +6,13 @@ const Hero = () => {
         <nav className='flex justify-between items-center w-full mb-10 pt-3'>
             <img src={logo} alt="logo" className='w-28 object-contain' />
             <button type='button' onClick={()=>window.open('https://github.com/mohammedsuhailkh')} className='black_btn'>Github</button>
-            
+
         </nav>
+        <h1 className='head_text'>
+            Summarize Articles With <br className='max-md:hidden'/>
+            <span className='orange_gradient'>OpenAi GPT-4</span>
+        </h1>
+        <h2 className="desc">Welcome to ShortnIt, the ultimate article shortening web app designed to make your reading experience more efficient and enjoyable. Whether you're a student, a professional, or simply someone who loves to read, ShortenIt is here to transform lengthy articles into concise and easily digestible content, without compromising the essence and essential information.</h2>
     </header>
   )
 }
